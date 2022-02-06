b2b28350f3653d9ef85fc3ebacfecf27c0fdebad
Judzhin Miles <info@msbios.com>
2022-02-06 14:58:20 +0200

    TODO Example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import "./App.css";
-import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import Loader from "./Loader";
+import Modal from "./Modal/Modal";
+import AddTodo from "./Todo/AddTodo";
+// import TodoList from "./Todo/TodoList";
+
+const TodoList = React.lazy(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(import("./Todo/TodoList"));
+      }, 0);
+    })
+);
 
 // function App() {
 //   return (
@@ -10,11 +23,14 @@ import TodoList from "./Todo/TodoList";
 
 // class App extends Component {
 function App() {
-  const [todos, setTodos] = React.useState([
-    { id: 1, completed: true, title: "Купить хлеб" },
-    { id: 2, completed: false, title: "Построить дом" },
-    { id: 3, completed: false, title: "Посадить дерево" },
-  ]);
+  // const [todos, setTodos] = React.useState([
+  //   { id: 1, completed: true, title: "Купить хлеб" },
+  //   { id: 2, completed: false, title: "Построить дом" },
+  //   { id: 3, completed: false, title: "Посадить дерево" },
+  // ]);
+
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
   // let todos = [
   //   { id: 1, completed: false, title: "Купить хлеб" },
@@ -30,6 +46,30 @@ function App() {
   //    </div>
   //   );
   // }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((response) => response.json())
+      // .then(json => console.log(json))
+      .then((todos) => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 2000);
+      });
+  }, []);
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false,
+        },
+      ])
+    );
+  }
 
   function toggleTodo(id) {
     // todos = todos.map(todo => {
@@ -48,11 +88,34 @@ function App() {
     );
   }
 
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
   return (
-    <div className="wrapper">
-      <h1>React tutorial</h1>
-      <TodoList todos={todos} onToggle={toggleTodo} />
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+
+        <Modal />
+
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo} />
+
+        {/* {loading && <Loader />} */}
+
+        {loading ? (
+          <Loader />
+        ) : todos.length ? (
+          // <TodoList todos={todos} onToggle={toggleTodo} />
+          // <React.Suspense fallback={<p>Please wait ...</p>}>
+          <React.Suspense fallback={<Loader />}>
+            <TodoList todos={todos} onToggle={toggleTodo} />
+          </React.Suspense>
+        ) : (
+          <p>No todos!</p>
+        )}
+      </div>
+    </Context.Provider>
   );
 }
 
---
+++ src/Loader.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Loader.css";
+
+export default () => (
+  <div style={{ display: "flex", justifyContent: "center", margin: ".5rem" }}>
+    <div className="loadingio-spinner-spinner-yh6wgwj7ewf">
+      <div className="ldio-q9iprheuzom">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+);
